8e09be12059eff74dccf4ebf9c4de3af91c5fc5a
zippyzippy0 <zkavutha403@gmail.com>
2025-09-15 15:29:23 +0300

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,11 +1,15 @@
-import osmnx as ox
+import os
 import warnings
-from typing import Dict, Optional
+import pandas as pd
+import torchaudio
+from torch.utils.data import Dataset
+
+# Existing geospatial imports
+import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore", category=FutureWarning, module="osmnx")
-
 
 class DataAccess:
     def __init__(self, place_name: str, latitude: float, longitude: float,
@@ -40,7 +44,7 @@ class DataAccess:
         self.edges = None
         self.buildings = None
 
-    def access_pois(self, tags: Optional[Dict] = None):
+    def access_pois(self, tags=None):
         if tags is None:
             tags = self.default_tags
         self.pois = ox.features_from_bbox(self.bbox, tags)
@@ -69,20 +73,9 @@ class DataAccess:
         except Exception:
             pass
 
-    def plot_city_map(self, zoom: int = 1, show_pois: bool = True,
-                      show_buildings: bool = True, show_roads: bool = True):
-        """
-        Plot a city map with optional POIs, buildings, and road networks.
-
-        Args:
-            zoom (int): scaling factor for map size.
-            show_pois (bool): if True, show Points of Interest.
-            show_buildings (bool): if True, show building footprints.
-            show_roads (bool): if True, show road networks.
-        """
+    def plot_city_map(self, zoom=1, show_pois=True, show_buildings=True, show_roads=True):
         fig, ax = plt.subplots(figsize=(8 * zoom, 8 * zoom))
 
-        # Plot area boundary
         if self.area is None:
             try:
                 self.access_area_boundary()
@@ -91,7 +84,6 @@ class DataAccess:
         if self.area is not None:
             self.area.plot(ax=ax, facecolor="none", edgecolor="black", linewidth=1)
 
-        # Plot buildings
         if show_buildings:
             if self.buildings is None:
                 try:
@@ -102,7 +94,6 @@ class DataAccess:
                 self.buildings.plot(ax=ax, facecolor="lightgray",
                                     edgecolor="gray", alpha=0.6)
 
-        # Plot roads
         if show_roads:
             if self.nodes is None or self.edges is None:
                 try:
@@ -113,7 +104,6 @@ class DataAccess:
                 self.edges.plot(ax=ax, linewidth=0.5,
                                 edgecolor="black", alpha=0.7)
 
-        # Plot POIs
         if show_pois:
             if self.pois is None:
                 try:
@@ -129,14 +119,57 @@ class DataAccess:
         plt.show()
 
 
-# --------------------------------------------------------------------
-# Standalone wrapper function
-# --------------------------------------------------------------------
 def plot_city_map(place_name: str, latitude: float,
                   longitude: float, zoom: int = 1):
-    """
-    Standalone utility to plot a city map without manually
-    creating a DataAccess instance.
-    """
     da = DataAccess(place_name, latitude, longitude)
     da.plot_city_map(zoom=zoom)
+
+
+
+class ESC50Dataset(Dataset):
+    def __init__(self, csv_path, audio_path, folds=None, sr=22050, n_mels=64, duration=5):
+        self.df = pd.read_csv(csv_path)
+        if folds is not None:
+            self.df = self.df[self.df["fold"].isin(folds)]
+        self.audio_path = audio_path
+        self.sr = sr
+        self.n_mels = n_mels
+        self.duration = duration
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        filepath = os.path.join(self.audio_path, row["filename"])
+        waveform, _ = torchaudio.load(filepath)
+        waveform = waveform.mean(dim=0, keepdim=True)
+
+        num_samples = self.duration * self.sr
+        if waveform.shape[1] < num_samples:
+            pad_size = num_samples - waveform.shape[1]
+            waveform = torch.nn.functional.pad(waveform, (0, pad_size))
+        else:
+            waveform = waveform[:, :num_samples]
+
+        mel_spec = torchaudio.transforms.MelSpectrogram(
+            sample_rate=self.sr,
+            n_mels=self.n_mels
+        )(waveform)
+
+        mel_spec_db = torchaudio.transforms.AmplitudeToDB()(mel_spec)
+        return mel_spec_db, row["target"]
+
+
+
+class XenoCantoAccess:
+    def __init__(self, metadata_csv, audio_path):
+        self.df = pd.read_csv(metadata_csv)
+        self.audio_path = audio_path
+
+    def list_species(self):
+        return self.df['en'].unique()
+
+    def get_recordings(self, species_name):
+        return self.df[self.df['en'] == species_name]
+
